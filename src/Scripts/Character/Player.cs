2ee47e072be157026b8b27be88e33492ee406784
NixyJuppie <git.nixy.carefully356@passfwd.com>
2024-05-29 22:36:14 +0200

    Add player movement and camera

---
+++ src/Scripts/Character/Player.cs
@@ -0,0 +1,22 @@
+namespace Anubis.Character;
+
+public partial class Player : CharacterBase
+{
+    public override void _Process(double delta)
+    {
+        var direction = Vector2.Zero;
+
+        if (Input.IsActionPressed("MoveUp"))
+            direction.Y--;
+        if (Input.IsActionPressed("MoveDown"))
+            direction.Y++;
+
+        if (Input.IsActionPressed("MoveLeft"))
+            direction.X--;
+        if (Input.IsActionPressed("MoveRight"))
+            direction.X++;
+
+        Velocity = direction * Speed;
+        MoveAndSlide();
+    }
+}
