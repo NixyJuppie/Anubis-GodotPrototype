803ccadc625ffba9a25f1529ce2093d6100eac88
NixyJuppie <git.nixy.carefully356@passfwd.com>
2024-05-31 17:18:55 +0200

    Update Godot to v4.3-beta1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Anubis
 
 ## Requirements
-- [Godot v4.3-dev6 (.NET)](https://godotengine.org/download/archive/4.3-dev6)
+- [Godot v4.3-beta1 (.NET)](https://godotengine.org/download/archive/4.3-beta1)
 - [.NET 8.0 SDK](https://dotnet.microsoft.com/download/dotnet/8.0)
--- src/Scripts/Characters/Player.cs
+++ src/Scripts/Characters/Player.cs
@@ -4,19 +4,9 @@ public partial class Player : Character
 {
     public override void _Process(double delta)
     {
-        var direction = Vector2.Zero;
-
-        if (Input.IsActionPressed("MoveUp"))
-            direction.Y--;
-        if (Input.IsActionPressed("MoveDown"))
-            direction.Y++;
-
-        if (Input.IsActionPressed("MoveLeft"))
-            direction.X--;
-        if (Input.IsActionPressed("MoveRight"))
-            direction.X++;
-
-        Velocity = direction * uint.Clamp(Agility.Value, 1, 100) * 50f;
+        var direction = Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown");
+        var multiplier = Input.IsActionPressed("Sprint") ? 50f : 25f;
+        Velocity = direction * uint.Clamp(Agility.Value, 1, 100) * multiplier;
         MoveAndSlide();
     }
 }
